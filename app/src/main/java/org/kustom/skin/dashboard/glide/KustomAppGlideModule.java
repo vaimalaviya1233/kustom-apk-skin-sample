c889e9247cbce67f45e7ce43498765331308f35f
Frank Monza <frank.bmonza@gmail.com>
2023-04-19 15:33:02 +0200

    removed BuildConfig from glide module

--- app/src/main/java/org/kustom/skin/dashboard/glide/KustomAppGlideModule.java
+++ app/src/main/java/org/kustom/skin/dashboard/glide/KustomAppGlideModule.java
@@ -3,18 +3,18 @@ package org.kustom.skin.dashboard.glide;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
-
-import whatever.kompany.skin.BuildConfig;
 
 @GlideModule
 public class KustomAppGlideModule extends AppGlideModule {
 
     @Override
-    public void applyOptions(Context context, GlideBuilder builder) {
-        builder.setLogLevel(BuildConfig.DEBUG ? Log.VERBOSE : Log.WARN);
+    public void applyOptions(@NonNull Context context, GlideBuilder builder) {
+        builder.setLogLevel(Log.WARN);
     }
 
 }

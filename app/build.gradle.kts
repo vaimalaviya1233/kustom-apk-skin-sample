e61573fc2423a15fe8da1b622582f717e3d6b42a
Frank Monza <frank.bmonza@gmail.com>
2023-04-20 17:22:40 +0200

    moved to kustom api 2

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+dependencies {
+    val libChannel: String by rootProject.extra
+    if (libChannel == "local") {
+        logger.warn("Configuring local libs")
+        implementation(project(":api"))
+        implementation(project(":preset"))
+        implementation(project(":dashboard"))
+    } else {
+        implementation("org.bitbucket.frankmonza:kustomapi:2.0")
+        implementation("org.bitbucket.frankmonza:kustompreset:2.0")
+        implementation("org.bitbucket.frankmonza:kustomdashboard:2.0")
+    }
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+}
+
+android {
+    compileSdk = rootProject.ext.get("compileSdkVersion") as Int
+    buildToolsVersion = rootProject.ext.get("buildToolsVersion") as String
+
+    defaultConfig {
+
+        /**
+         * CHANGE THIS TO YOUR APP PKG NAME THIS CANNOT BE CHANGED AFTER FIRST RELEASE
+         */
+        applicationId = "whatever.kompany.skin"
+
+        /**
+         * INCREMENT THIS IF YOU WANT TO RELEASE UPDATES
+         */
+        versionCode = 1
+        versionName = "1"
+
+        minSdk = 23
+        targetSdk = rootProject.ext.get("targetSdkVersion") as Int
+    }
+
+    buildTypes {
+        release {
+            isShrinkResources = false
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules")
+        }
+        aaptOptions.setNoCompress("zip", "komp", "klwp", "kwgt", "klck", "kwch", "kntf")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+
+    namespace = "whatever.kompany.skin"
+}
---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+    }
+}
+
+extra["libChannel"] = "official"
+
+allprojects {
+    allprojects {
+        repositories {
+            mavenCentral()
+            google()
+        }
+        ext {
+            set("buildToolsVersion", "33.0.2")
+            set("compileSdkVersion", 33)
+            set("targetSdkVersion", 33)
+        }
+    }
+}
+
